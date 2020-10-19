8b85579b419b08cb0c7fc9a91a0567041bb7fd88
syakoo <sakodata0318@gmail.com>
2020-10-19 15:57:40 +0900

    add: :tada:

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,43 +1,31 @@
-'use strict';
+"use strict";
 
-// Content script file will run in the context of web page.
-// With content script you can manipulate the web pages using
-// Document Object Model (DOM).
-// You can also pass information to the parent extension.
+import html2canavs from "html2canvas";
 
-// We execute this script by making an entry in manifest.json file
-// under `content_scripts` property
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  downloadTargetNode(msg);
 
-// For more information on Content Scripts,
-// See https://developer.chrome.com/extensions/content_scripts
-
-// Log `title` of current active web page
-const pageTitle = document.head.getElementsByTagName('title')[0].innerHTML;
-console.log(
-  `Page title is: '${pageTitle}' - evaluated by Chrome extension's 'contentScript.js' file`
-);
-
-// Communicate with background file by sending a message
-chrome.runtime.sendMessage(
-  {
-    type: 'GREETINGS',
-    payload: {
-      message: 'Hello, my name is Con. I am from ContentScript.',
-    },
-  },
-  response => {
-    console.log(response.message);
-  }
-);
-
-// Listen for message
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'COUNT') {
-    console.log(`Current count is ${request.payload.count}`);
-  }
-
-  // Send an empty response
-  // See https://github.com/mozilla/webextension-polyfill/issues/130#issuecomment-531531890
   sendResponse({});
   return true;
 });
+
+const downloadTargetNode = (targetNodeTxt) => {
+  const a = document.createElement("a");
+  a.setAttribute("download", `download.png`);
+
+  const targetNode = document.querySelector(targetNodeTxt);
+  if (!targetNode) {
+    console.error("対象のノードが見つかりませんでした👻", { targetNodeTxt });
+    return;
+  }
+
+  scrollTo(0, 0);
+  html2canavs(targetNode).then((canvas) => {
+    console.log({ canvas });
+    canvas.toBlob((blob) => {
+      a.setAttribute("href", window.URL.createObjectURL(blob));
+      a.click();
+      console.log("clicked");
+    });
+  });
+};
